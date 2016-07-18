84c5372bf1341833d86943a1e0abf01319293b6c
Giulio Michelon <giulio@belka.us>
2016-07-18 14:23:51 +0200

    typing animation in homepage

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var sass        = require('gulp-sass');
 var prefix      = require('gulp-autoprefixer');
 var cp          = require('child_process');
 var deploy      = require("gulp-gh-pages");
+var concat      = require('gulp-concat');
 
 
 var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
@@ -14,23 +15,23 @@ var messages = {
 /**
  * Build the Jekyll Site
  */
-gulp.task('jekyll-build', function (done) {
+ gulp.task('jekyll-build', function (done) {
     browserSync.notify(messages.jekyllBuild);
     return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
-        .on('close', done);
+    .on('close', done);
 });
 
 /**
  * Rebuild Jekyll & do page reload
  */
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+ gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
     browserSync.reload();
 });
 
 /**
  * Wait for jekyll-build, then launch the Server
  */
-gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
+ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
     browserSync({
         server: {
             baseDir: '_site'
@@ -38,26 +39,35 @@ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
     });
 });
 
+/** 
+* Concat the js files needed for production
+*/ 
+gulp.task('concat', function() {
+    return gulp.src(['node_modules/typed.js/dist/typed.min.js','js/site.js'])
+  .pipe(concat('all.js'))
+  .pipe(gulp.dest('js/'));
+});
+
 /**
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
  */
-gulp.task('sass', function () {
+ gulp.task('sass', function () {
     return gulp.src('_scss/main.scss')
-        .pipe(sass({
-            includePaths: ['scss'],
-            onError: browserSync.notify
-        }))
-        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest('_site/css'))
-        .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('css'));
+    .pipe(sass({
+        includePaths: ['scss'],
+        onError: browserSync.notify
+    }))
+    .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+    .pipe(gulp.dest('_site/css'))
+    .pipe(browserSync.reload({stream:true}))
+    .pipe(gulp.dest('css'));
 });
 
 /**
  * Watch scss files for changes & recompile
  * Watch html/md files, run jekyll & reload BrowserSync
  */
-gulp.task('watch', function () {
+ gulp.task('watch', function () {
     gulp.watch('_scss/*.scss', ['sass']);
     gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
@@ -66,11 +76,11 @@ gulp.task('watch', function () {
  * Default task, running just `gulp` will compile the sass,
  * compile the jekyll site, launch BrowserSync & watch files.
  */
-gulp.task('default', ['browser-sync', 'watch']);
+ gulp.task('default', ['browser-sync', 'watch', 'concat']);
 
 
 
-gulp.task("deploy", ["jekyll-build"], function () {
+ gulp.task("deploy", ["jekyll-build"], function () {
     return gulp.src("./_site/**/*")
-      .pipe(deploy());
+    .pipe(deploy());
 });
---
+++ js/site.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+	$(function(){
+	    $("#claim-text").typed({
+	    	strings: ["Tasty tech events in ^300 Trento."],
+	        typeSpeed: 0
+	    });
+	});
+});
