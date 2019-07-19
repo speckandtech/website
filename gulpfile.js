4601ab67eda4403e9f6064f0fa6eb7d380b451d0
SamDja <samuelgiacomelli@gmail.com>
2019-07-19 14:21:08 +0200

    BIG changes - updated dependencies and gulpfile, also added new event

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp        = require('gulp');
-var browserSync = require('browser-sync');
+var browser_sync = require('browser-sync');
 var sass        = require('gulp-sass');
 var prefix      = require('gulp-autoprefixer');
-var cp          = require('child_process');
-var deploy      = require("gulp-gh-pages");
+var deployPage      = require("gulp-gh-pages");
 var concat      = require('gulp-concat');
+const { series } = require('gulp');
+const { parallel } = require('gulp');
+const { spawn } = require('child_process');
 
 
 var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
@@ -12,76 +14,75 @@ var messages = {
     jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
 };
 
-/**
- * Build the Jekyll Site
- */
- gulp.task('jekyll-build', function (done) {
-    browserSync.notify(messages.jekyllBuild);
-    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
-    .on('close', done);
-});
+function jekyllExec() {
+    browser_sync.notify(messages.jekyllBuild);
+    return spawn(jekyll, ['build'], { stdio: "inherit" });
+}
 
-/**
- * Rebuild Jekyll & do page reload
- */
- gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
-    browserSync.reload();
-});
-
-/**
- * Wait for jekyll-build, then launch the Server
- */
- gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
-    browserSync({
+function browserSyncStart(done) {
+    browser_sync({
         server: {
-            baseDir: '_site'
-        }
+            baseDir: '_site',
+        },
+        port: 8080
     });
-});
+    done();
+}
 
-/**
-* Concat the js files needed for production
-*/
-gulp.task('concat', function() {
-    return gulp.src(['node_modules/typed.js/dist/typed.min.js','js/site.js'])
-  .pipe(concat('all.js'))
-  .pipe(gulp.dest('js/'));
-});
+function browserSyncReload(done) {
+    browser_sync.reload();
+    done();
+}
+
+// /**
+// * Concat the js files needed for production
+// */
+function concatJS () {
+    return gulp.src(['js/typed.min.js','js/site.js'])
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('js/'));
+}
 
 /**
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
  */
- gulp.task('sass', function () {
-    return gulp.src('_scss/main.scss')
+function buildSass () {
+    return gulp.src('./_scss/main.scss')
     .pipe(sass({
-        includePaths: ['scss'],
-        onError: browserSync.notify,
+        includePaths: ['./_scss'],
+        onError: browser_sync.notify,
         outputStyle: 'compressed'
     }))
     .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
     .pipe(gulp.dest('_site/css'))
-    .pipe(browserSync.reload({stream:true}))
+    .pipe(browser_sync.reload({stream:true}))
     .pipe(gulp.dest('css'));
-});
+}
 
-/**
- * Watch scss files for changes & recompile
- * Watch html/md files, run jekyll & reload BrowserSync
- */
- gulp.task('watch', function () {
-    gulp.watch('_scss/*.scss', ['sass']);
-    gulp.watch(['*.html', '_layouts/*.html', '_includes/**', '_posts/*', '*.md'], ['jekyll-rebuild']);
-});
+function watchAll () {
+    // Watch files
+    gulp.watch('./_scss/*.scss', series(buildSass, browserSyncReload));
+    gulp.watch(['./js/*.js', '!./js/all.js'], series(concatJS, browserSyncReload));
+    gulp.watch(
+      [
+        '*.html',
+        './_includes/**/*',
+        './_layouts/**/*',
+        './_posts/**/*',
+        './img/**/*',
+    ], series(jekyllExec, browserSyncReload));
+    gulp.watch('./js/*.js', browserSyncReload);
+}
 
-/**
- * Default task, running just `gulp` will compile the sass,
- * compile the jekyll site, launch BrowserSync & watch files.
- */
- gulp.task('default', ['browser-sync', 'watch', 'concat']);
+function deployGhPages (cb) {
+    gulp.src('./_site/**/*')
+    .pipe(deployPage());
+    cb();
+}
 
+const build = parallel(buildSass, concatJS, jekyllExec);
 
-
- gulp.task("deploy", ["jekyll-build"], function () {
-    return gulp.src("./_site/**/*")
-    .pipe(deploy());
-});
+exports.deploy = series(build, deployGhPages);
+exports.build = build;
+exports.js = concatJS;
+exports.default = series(build, parallel(watchAll, browserSyncStart));
