6175d72153fd99d025687ea2337a66c69c038302
SamDja <samuelgiacomelli@gmail.com>
2019-07-31 16:22:15 +0200

    Solved some style issues

--- js/retreat.js
+++ js/retreat.js
@@ -1,13 +1,22 @@
 $( document ).ready(function() {
 
+    var timeout = 2000;
+
     // $("visitorNumber").text("Altre "+visitors+" persone stanno guardando questo sito");
     function calcVisitors() {
         var visitors = Math.max(Math.round(Math.random()*8),2);
-        document.getElementById("visitorNumber").innerHTML = "<p>"+"Altre <strong>"+visitors+"</strong> persone stanno guardando il sito in questo momento"+"</p>";
+        var text = "<p>"+"Altre <strong>"+visitors+"</strong> persone stanno guardando il sito in questo momento 👀"+"</p>";
+        if (Math.random() > 0.6) {
+            text = "<p>🐷</p>";
+            timeout = 5000;
+        } else {
+            timeout = 2000;
+        }
+        document.getElementById("visitorNumber").innerHTML = text;
     }
 
     calcVisitors();
 
-    setInterval(calcVisitors, 10000);
+    setInterval(calcVisitors, timeout);
 
 });
