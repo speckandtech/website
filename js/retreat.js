3156785fe396aa630c39df6ec1b3262d5e71e04f
SamDja <samuelgiacomelli@gmail.com>
2019-08-01 14:42:34 +0200

    Added background image to retreat page on small screens

--- js/retreat.js
+++ js/retreat.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
     // $("visitorNumber").text("Altre "+visitors+" persone stanno guardando questo sito");
     function calcVisitors() {
         var visitors = Math.max(Math.round(Math.random()*8),2);
-        var text = "<p>"+"Altre <strong>"+visitors+"</strong> persone stanno guardando il sito in questo momento 👀"+"</p>";
+        var text = "<p>"+"<strong>"+visitors+"</strong> people are watching this page 👀"+"</p>";
         if (Math.random() > 0.6) {
             text = "<p>🐷</p>";
             timeout = 5000;
